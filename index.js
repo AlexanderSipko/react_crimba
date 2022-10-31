1e20e24773a43fac9239344d81b6ce87bbade1f8
Asipko <avsipko@gmail.com>
2022-10-31 22:40:07 +0300

    ferst commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function MainContin() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+ReactDOM.render(
+<div>
+    {/* <Navbar/> */}
+    <MainContin/>
+</div>
+, document.querySelector('#root'))
