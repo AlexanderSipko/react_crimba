80275eb2ac9badda33323bfe801cf1b9b9898bb6
Asipko <avsipko@gmail.com>
2022-11-01 13:02:59 +0300

    last change

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 /* 
     - Create a new h1 lement
     - Give it some textContnetn
@@ -12,5 +11,14 @@
 // h1.classList.add('header')
 // document.querySelector('#root').append(h1)
 
+const element = (
+    <div>
+        <h1>Hi everyone</h1>
+        <h1 className='header'>I was created React</h1>  
+        <p><a href="https://www.youtube.com/watch?v=bMknfKXIFA8">
+            stopping time 38 "Build a React info site Goodbye, CDNs!"</a></p>
+    </div>
+)
 
-ReactDOM.render(<h1 className='header'>I was created React</h1>, document.querySelector('#root'))
+
+ReactDOM.render(element, document.querySelector('#root'))
