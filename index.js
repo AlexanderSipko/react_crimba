5bd7e51e92be1907ec0b5f1e299f680f0860e58a
Asipko <avsipko@gmail.com>
2022-11-01 07:05:43 +0300

    learn second

--- index.js
+++ index.js
@@ -7,7 +7,10 @@
     - append it as child of the div#root  
 */
 
-const h1 = document.createElement('h1')
-h1.textContent = 'Some text there'
-h1.classList.add('header')
-document.querySelector('#root').append(h1)
+// const h1 = document.createElement('h1')
+// h1.textContent = 'Some text there'
+// h1.classList.add('header')
+// document.querySelector('#root').append(h1)
+
+
+ReactDOM.render(<h1 className='header'>I was created React</h1>, document.querySelector('#root'))
