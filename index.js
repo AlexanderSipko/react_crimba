f27a5132fb55d7f77c039fd349abf938477914c0
Asipko <avsipko@gmail.com>
2022-11-09 23:26:57 +0300

    new lesson

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 
-/*
-9. Build a react info site
-    Goodby, CDNs
-*/
 
-// import React from "react"
-// import ReactDOM from "react-dom"
+function Temp() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <ul>
+                <li>components</li>
+            </ul>
+        </div>
+    )
+}
 
-const page = (
-   <div>
-       <h1>My awesome website in React</h1>
-       <h3>Reason i love React</h3>
-       <ol>
-           <li>It's composable</li>
-           <li>It's declarative</li>
-           <li>It's a hareableskill</li>
-           <li>It's actively maintained by skilled people</li>
-       </ol>
-   </div>
-)
 
-ReactDOM.render(page, document.querySelector('#root'))
-// document.querySelector('#root').append(JSON.stringify(page))
+
+
+ReactDOM.render(<Temp/>, document.querySelector('#root'))
